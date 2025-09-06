088a4a3832ec1e6dcedb87ec003905d83cb14a84
PlayBi <thangcattle@gmail.com>
2025-09-07 01:50:05 +0700

    feat: setup prisma with postgreSQL

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  // eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -28,7 +28,10 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"],
+      "prettier/prettier": ["error", { semi: true, singleQuote: true }],
     },
   },
 );
